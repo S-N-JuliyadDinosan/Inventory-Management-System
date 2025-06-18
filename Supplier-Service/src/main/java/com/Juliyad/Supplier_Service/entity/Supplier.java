2daa43e077b387e9f926340ed34b1645dc0d21f6
S N Juliyad Dinosan <brackdino90@gmail.com>
2025-06-18 13:33:22 +0530

    Spplier Entity created

--- Supplier-Service/src/main/java/com/Juliyad/Supplier_Service/entity/Supplier.java
+++ Supplier-Service/src/main/java/com/Juliyad/Supplier_Service/entity/Supplier.java
@@ -1,4 +1,21 @@
 package com.Juliyad.Supplier_Service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "suppliers")
 public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long supplierId;
+    private String name;
+    private String contactEmail;
+
 }
